a5e40a2680dbe7d10818536faa257217348f3eeb
komus-Israel <komolehinIsrael@gmail.com>
2021-09-13 22:34:18 +0100

    fetching registration categories from endpoint

--- src/components/register_business.js
+++ src/components/register_business.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Checkbox } from '@material-ui/core';
 import { RadioGroup, Radio } from '@material-ui/core';
 
 
@@ -8,8 +7,7 @@ import { RadioGroup, Radio } from '@material-ui/core';
 
 const BusinessRegisteration=()=>{
 
-    const [businessimages, setBusinessImages] = useState([]);
-    const [businessLogo, setBusinessLogo] = useState([]);
+    
     const [first_name, setFirstName] = useState('')
     const [last_name, setLastName] = useState('')
     const [email, setEmail] = useState('')
@@ -21,11 +19,10 @@ const BusinessRegisteration=()=>{
     const [website_url, setWebsiteUrl] = useState('')
     const [business_description, setDescription] = useState('')
     const [category, setCategory] = useState('')
-    const [passwordMatch, setPasswordMatch] = useState(true)
-    const [registerationResponse, setRegisteraionResponse] = useState([])
-    const [isPending, setIspending] = useState(false)
-    const [registrationComplete, setRegistrationComplete] = useState(false)
-    const [businessData, setBusinessData] = useState({})
+    const [categories, setCategories] = useState([])
+    
+    const prod = 'https://kwaralive.herokuapp.com'
+    const local = 'http://localhost:5000'
 
 
     const showCategories=()=>{
@@ -33,133 +30,31 @@ const BusinessRegisteration=()=>{
         categoryContainer.style.display = 'block'
     }
 
-    const categories = [
-        {
-          category: "Interior design", 
-          id: 7   
-        }, 
-        {
-          category: "Government", 
-          id: 8 
-        }, 
-        {
-          category: "Entertainment", 
-          id: 9
-        }, 
-        {
-            category: "Logistics", 
-            id: 10
-        }, 
-        {
-            category: "Photography", 
-            id: 11
-        }, 
-        {
-            category: "Media", 
-            id: 12
-        }, 
-        {
-            category: "Car Dealer", 
-            id: 14
-        }, 
-        {
-            category: "Security", 
-            id: 16
-        }, 
-        {
-            category: "Radio Station", 
-            id: 17
-        }, 
-        {
-            category: "Telecoms", 
-            id: 18
-        }, 
-        {
-            category: "Studio", 
-            id: 19
-        }, 
-        {
-            category: "Survey", 
-            id: 20
-        }, 
-        {
-            category: "Fishery", 
-            id: 23
-        }, 
-        {
-            category: "Boutique and Jewelries", 
-            id: 24
-        }, 
-        {
-            category: "Boutique", 
-            id: 25
-        }, 
-        {
-            category: "FARM FEEDS & FARMERS", 
-            id: 26
-        }, 
-        {
-            category: "HOTELS", 
-            id: 27
-        }, 
-        {
-            category: "ICT", 
-            id: 28
-        }, 
-        {
-            category: "PHARMACY", 
-            id: 29
-        }, 
-        {
-            category: "COLD ROOM & FROZEN FOOD", 
-            id: 30
-        }, 
-        {
-            category: "CONFECTIONERIES/GRILLS", 
-            id: 31
-        }, 
-        {
-            category: "COSMETICS STORES", 
-            id: 32
-        }, 
-        {
-            category: "EDUCATIONAL CONSULTS & SCHOOLS", 
-            id: 33
-        }, 
-        {
-            category: "FASHION", 
-            id: 34
-        }, 
-        {
-            category: "GAS REFILL", 
-            id: 35
-        }, 
-        {
-            category: "Property Construction and consultant", 
-            id: 6
-        }, 
-        {
-            category: "Agriculture", 
-            id: 21
-        }, 
-        {
-            category: "Agro Chemical", 
-            id: 22
-        }, 
-        {
-            category: "Automobile", 
-            id: 13
-        }, 
-        {
-            category: "Beauty", 
-            id: 15
-        }
-      ]
+    useEffect(()=>{
+        fetch(
+            prod + '/v1/business-categories',
+            {
+                method: 'GET',
+                headers:{
+                    'Content-type':'application/json',
+                    crossDomain:true       
+                }
+            }
+        ).then(
+            response => {
+                if (response.ok){
+                    return response.json()
+                }
+            }
+        ).then(
+            data => {
+                setCategories(data.categories)
+                console.log(categories)
+            }
+            
+        )
+    }, [])
     
-  
-      
-    
-      
     return(
         <div className='user-form-cont'>
 
